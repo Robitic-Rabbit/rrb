40a08da1b3a7894874b4a306df02b969b0b518e6
Chethani Saumya <105449384+ChethaniSaumya@users.noreply.github.com>
2025-04-06 17:17:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -178,7 +178,7 @@ app.use((req, res, next) => {
 });
 
 var corsOptions = {
-    origin: ['https://d2mlmfod4h1sc4.cloudfront.net/', 'https://adminrabbit.vercel.app/'],
+    origin: ['https://d2mlmfod4h1sc4.cloudfront.net/', 'https://adminrabbit.vercel.app/', 'https://frontend-check.vercel.app/'],
     optionsSuccessStatus: 200,
     methods: "GET,POST",
     allowedHeaders: ["Content-Type"],
