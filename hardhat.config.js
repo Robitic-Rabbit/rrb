3740d7ef6d79c85e20b793a5f3c647b7910ca895
Chethani Saumya <105449384+ChethaniSaumya@users.noreply.github.com>
2025-04-27 23:35:19 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,19 +1,19 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 require("@nomicfoundation/hardhat-toolbox");
-//const dotenv = require('dotenv');
-//dotenv.config();
+const dotenv = require('dotenv');
+dotenv.config();
 
 const signer = process.env.SIGN;
 const SCANPOLY = process.env.SCANPOLY;
-const SCANPULSE = process.env.SCANPULSE;
-//console.log("read signer : " + signer);
+
+console.log("read signer : " + signer);
 
 
 module.exports = {
   solidity: "0.8.19",
   networks: {
     polygonMumbai: {
-      url: "https://holy-burned-sheet.optimism.quiknode.pro/f7e9efbb6f5a0385ae4c3a55b94bb67eca5e31bc/",
+      url: "https://base-mainnet.infura.io/v3/50597910853247b38793be4ec6b05dc8",
       //url: "https://pulsechain-rpc.publicnode.com",
       accounts: [signer],
     },
